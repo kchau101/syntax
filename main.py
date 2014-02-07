13d41cfa8c19e3c9f952ed08343e33c935b60bf3
Jack <jack@cirno.de>
2014-02-07 14:58:45 -0500

    Added a menubar

--- main.py
+++ main.py
@@ -23,25 +23,59 @@
 #  
 
 import wx
-        
+print wx.version()
 
 class MainUI_Syntax(wx.Frame):
     def __init__(self, *args, **kwargs):
-         pass           
-
+        super(MainUI_Syntax, self).__init__(*args, **kwargs)
+        self.Centre()
+        self.__Generate_Menubar()
+    def __Generate_Menubar(self):
+        self.Menubar = wx.MenuBar()
+        self.MenubarFile = wx.Menu()
+        self.MenubarEdit = wx.Menu()
+        self.MenubarHelp = wx.Menu()
+		
+        id_FILE_OPEN = wx.NewId()
+        id_FILE_QUIT = wx.NewId()
+        id_HELP_ABOUT = wx.NewId()
+		
+        self.MenubarFile.Append(id_FILE_OPEN, "&Open", "Open a file")
+        self.MenubarFile.AppendSeparator()
+        self.MenubarFile.Append(wx.ID_EXIT, "&Quit", "Quit the Program")
+		
+        self.MenubarHelp.Append(wx.ID_ABOUT, "About")
+		
+        self.Menubar.Append(self.MenubarFile, "File")
+        self.Menubar.Append(self.MenubarEdit, "Edit")
+        self.Menubar.Append(self.MenubarHelp, "Help")
+		
+        self.Bind(wx.EVT_MENU, self.OnQuit, id=wx.ID_EXIT)
+        self.Bind(wx.EVT_MENU, self.OnAbout, id=wx.ID_ABOUT)
+        
+        self.SetMenuBar(self.Menubar)
+    def OnQuit(self, event):
+        self.Close()
+    def OnAbout(self, event):
+        helpDialog = wx.MessageDialog(self, "An Open Source Language Learning Program", "Syntax", wx.OK)
+        helpDialog.ShowModal()
+        helpDialog.Destroy()
 class App_Syntax(wx.App):
+    loading = True
+	
     def OnInit(self):
         """ Generate a splash screen while loading assets """
         self.img = wx.Image("images/splash.jpg")
         self.img = wx.BitmapFromImage(self.img)
         self.splashscreen = wx.SplashScreen(self.img, 
             wx.SPLASH_CENTRE_ON_SCREEN, 2000, None)
+        self.UI = MainUI_Syntax(None, wx.ID_ANY, "Syntax")
+        self.UI.Show(True)
+        self.splashscreen.Destroy()
         return True
 
 def main():
     app = App_Syntax()
-    frame = wx.Frame(None, wx.ID_ANY, "Hello World")
-    frame.Show(True)
     app.MainLoop()
     return 0
 
